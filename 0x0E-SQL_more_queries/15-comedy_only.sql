4acff021da823296687a22e3d8be36eebaeb124f
patoctd4real <patoctd4real@yahoo.com>
2022-11-30 15:54:42 -0800

    updated

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Lists all comedy shows in the database hbtn_0d_tvshows.
 -- Records are ordered by descending show title.
-[2;2R[>77;30601;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000SELECT t.`title`
+SELECT t.`title`
   FROM `tv_shows` AS t
        INNER JOIN `tv_show_genres` AS s
        ON t.`id` = s.`show_id`
